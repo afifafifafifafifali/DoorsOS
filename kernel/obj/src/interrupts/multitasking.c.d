9a226192fdad6de54a145f7a92c04f3a84e693fe
Afif Ali Saadman <afifsaadman2013@gmail.com>
2025-09-07 21:39:48 +0600

    Summer Of Making

--- kernel/obj/src/interrupts/multitasking.c.d
+++
@@ -1,4 +0,0 @@
-obj/src/interrupts/multitasking.c.o: src/interrupts/multitasking.c \
- src/interrupts/multitasking.h src/interrupts/../gfx/printf.h
-src/interrupts/multitasking.h:
-src/interrupts/../gfx/printf.h:
--- kernel/obj/src/interrupts/timer.c.d
+++
@@ -1,4 +0,0 @@
-obj/src/interrupts/timer.c.o: src/interrupts/timer.c \
- src/interrupts/timer.h src/interrupts/../gfx/printf.h
-src/interrupts/timer.h:
-src/interrupts/../gfx/printf.h:
